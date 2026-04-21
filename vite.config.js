544e2b53b283219cdd85020927386487a0b27389
vinothkumar sanjeevi <vinodjayasudha@gmail.com>
2026-04-21 11:55:42 +0100

    final fix remove broken vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      react: 'react/index.js',
-      'react/jsx-runtime': 'react/jsx-runtime.js'
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react/jsx-runtime']
-  }
+  plugins: [react()]
 })
